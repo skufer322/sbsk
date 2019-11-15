5cefbf2c87e73875deff986099f795f22440b31d
Stefan Kufer <stefan.kufer@diva-e.com>
2019-11-15 08:51:44 +0100

    fixed failing tests

--- src/main/java/com/divae/sk/sbsk/controller/httpsecurity/CustomHttpSecurityConfig.java
+++ src/main/java/com/divae/sk/sbsk/controller/httpsecurity/CustomHttpSecurityConfig.java
@@ -1,10 +1,12 @@
 package com.divae.sk.sbsk.controller.httpsecurity;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
+@Profile("!disable-security")
 public class CustomHttpSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
